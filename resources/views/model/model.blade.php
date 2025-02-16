091a15ada347c927fc4ffb35ce66d0f6bcabcdc0
aszaychik <aszaychik@gmail.com>
2025-02-16 09:06:32 +0700

    feat(model): enhance evaluation status display with conditional formatting

--- resources/views/model/model.blade.php
+++ resources/views/model/model.blade.php
@@ -95,7 +95,17 @@
                                 {{ $evaluation->total }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ $evaluation->status }}
+                                @if ($evaluation->status == true)
+                                    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                        Terima
+                                    </span>
+
+                                @else
+                                    <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100">
+                                        Tolak
+                                    </span>
+
+                                @endif
                             </td>
                         </tr>
                     @endforeach
