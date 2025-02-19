90d796407d2ffa13ff234cf2606583090d9b8a8c
aszaychik <aszaychik@gmail.com>
2025-02-19 23:22:55 +0700

    feat(wawancara): add automatic Penilaian status prediction after save

--- app/Filament/Resources/WawancaraResource/Pages/EditWawancara.php
+++ app/Filament/Resources/WawancaraResource/Pages/EditWawancara.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\WawancaraResource\Pages;
 
 use App\Filament\Resources\WawancaraResource;
+use App\Http\Controllers\ModelController;
+use App\Models\Penilaian;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,5 +17,25 @@ class EditWawancara extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        parent::afterSave();
+
+        if ($this->record->nilai != 0) {
+            $penilaian = Penilaian::create([
+                'pelamar_id' => $this->record->pelamar_id,
+            ]);
+
+
+            $modelController = new ModelController();
+
+            $predictedStatus = $modelController->calculateC45($penilaian);
+
+            $penilaian->update([
+                'status' => $predictedStatus,
+            ]);
+        }
     }
 }
