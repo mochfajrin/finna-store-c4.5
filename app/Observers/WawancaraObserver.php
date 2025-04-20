37d57fb3d392368fb899dd4a964eed5d280b8f25
mochfajrin <mochfajrin1211@gmail.com>
2025-04-20 19:02:32 +0700

    feat: notification result table

--- app/Observers/WawancaraObserver.php
+++ app/Observers/WawancaraObserver.php
@@ -8,6 +8,7 @@ use App\Models\Notification;
 use App\Models\Penilaian;
 use App\Models\Wawancara;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
@@ -40,6 +41,39 @@ class WawancaraObserver
             'year' => Carbon::now()->year,
             'status' => $penilaian->status
         ];
+
+        $dataPelamar = DB::table('pelamars as p')
+            ->leftJoin('tes as t', 'p.id', '=', 't.pelamar_id')
+            ->leftJoin('evaluasis as e', 'p.id', '=', 'e.pelamar_id')
+            ->leftJoin('kriterias as k', 'e.kriteria_id', '=', 'k.id')
+            ->leftJoin('wawancaras as w', 'p.id', '=', 'w.pelamar_id')
+            ->leftJoin('penilaians as n', 'p.id', '=', 'n.pelamar_id')
+            ->select([
+                'p.id as pelamar_id',
+                'p.nama as nama',
+                DB::raw("MAX(CASE WHEN k.judul = 'riwayat' THEN e.nilai END) as riwayat"),
+                DB::raw("MAX(CASE WHEN k.judul = 'ktp' THEN e.nilai END) as ktp"),
+                DB::raw("MAX(CASE WHEN k.judul = 'skck' THEN e.nilai END) as skck"),
+                DB::raw("MAX(CASE WHEN k.judul = 'ijazah' THEN e.nilai END) as ijazah"),
+                DB::raw("MAX(CASE WHEN t.jenis = 'buta_warna' THEN t.nilai END) as buta_warna"),
+                DB::raw("MAX(CASE WHEN t.jenis = 'kemampuan' THEN t.nilai END) as kemampuan"),
+                'w.nilai as wawancara',
+                'n.status as status',
+                DB::raw("
+        COALESCE(MAX(CASE WHEN k.judul = 'riwayat' THEN e.nilai END), 0) +
+        COALESCE(MAX(CASE WHEN k.judul = 'ktp' THEN e.nilai END), 0) +
+        COALESCE(MAX(CASE WHEN k.judul = 'skck' THEN e.nilai END), 0) +
+        COALESCE(MAX(CASE WHEN k.judul = 'ijazah' THEN e.nilai END), 0) +
+        COALESCE(MAX(CASE WHEN t.jenis = 'buta_warna' THEN t.nilai END), 0) +
+        COALESCE(MAX(CASE WHEN t.jenis = 'kemampuan' THEN t.nilai END), 0) +
+        COALESCE(w.nilai, 0) +
+        COALESCE(n.status, 0) AS total")
+            ])
+            ->where('p.id', $penilaian->pelamar_id)
+            ->groupBy('p.id', 'p.nama', 'w.nilai', 'n.status')
+            ->first();
+
+
         // Mail::to($penilaian->pelamar->email)->send(new SendResultEmail($data));
         Notification::create([
             'title' => "Pemberitahuan penerimaan kerja lowongan {$penilaian->pelamar->lowongan->judul}",
@@ -76,6 +110,70 @@ class WawancaraObserver
                     ' . config('app.name') . '
                 </p>
             </div>') . '
+        <div class="relative overflow-x-auto">
+            <div class="text-center">
+                <h1><strong>Hasil Penilaian</strong></h1>
+            </div>
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Riwayat
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            KTP
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            SKCK
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Ijazah
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            buta Warna
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Kemampuan
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Wawancara
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Total
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->riwayat . '
+                        </th>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->ktp . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->skck . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->ijazah . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->buta_warna . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->kemampuan . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->wawancara . '
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            ' . $dataPelamar->total . '
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     </div>
             '
         ]);
